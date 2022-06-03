47e67b8c1adda32a385b0fa50f330d48637c9b7f
João Ferreira <joaoferreirape@gmail.com>
2022-06-03 08:41:50 -0300

    Corrigindo o projeto

--- GRP08/urnaz/src/app/tab2/tab2.page.ts
+++ GRP08/urnaz/src/app/tab2/tab2.page.ts
@@ -6,10 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab2.page.scss']
 })
 export class Tab2Page {
-  
+
+  public voto: number;
+  public votos10: number = 0;
+  public votos20: number = 0;
+
   constructor() {}
 
-  votar(voto){
-    return console.log(voto);
+  votar(voto: number) {
+    this.voto = voto;
+
+    if (this.voto == 10) {
+      this.votos10++;
+      console.log('Votou no 10.');
+    } else if (this.voto == 20) {
+      this.votos20++;
+      console.log('Votou no 20.');
+    } else {
+      console.log('Voto inválido.');
+    }
+
   }
 }
